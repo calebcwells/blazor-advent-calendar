c68b903560d75f26da860af1d7669bb7833eaa8a
Caleb Wells <calebcwells@outlook.com>
2020-12-17 12:39:15 -0600

    Fleshed out present list and added list of urls

--- BlazorAdventCalendar/Services/PresentRepository.cs
+++ BlazorAdventCalendar/Services/PresentRepository.cs
@@ -2,19 +2,55 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace BlazorAdventCalendar.Services
 {
     public class PresentRepository : IPresentRepository
     {
         private List<Present> presents;
+        private List<Resource> resources;
         public PresentRepository()
         {
-            presents = new List<Present> { 
-                new Present { Id = 1, Name = "First day of Christmas", Url = "https://dotnet.microsoft.com/learn/aspnet/blazor-tutorial/intro", Date = new DateTime(2020, 12, 1) },
-                new Present { Id = 2, Name = "Second day of Christmas", Url = "https://dotnet.microsoft.com/learn/aspnet/blazor-tutorial/intro", Date = new DateTime(2020, 12, 2) },
-                new Present { Id = 3, Name = "Third day of Christmas", Url = "https://dotnet.microsoft.com/learn/aspnet/blazor-tutorial/intro", Date = new DateTime(2020, 12, 3) },
+            presents = new List<Present> {
+                new Present { Id = 1, Name = "Twenty-four days until Christmas", Url = "", Date = new DateTime(2020, 12, 1) },
+                new Present { Id = 2, Name = "Twenty-three days until Christmas", Url = "", Date = new DateTime(2020, 12, 2) },
+                new Present { Id = 3, Name = "Twenty-two days until Christmas", Url = "", Date = new DateTime(2020, 12, 3) },
+                new Present { Id = 4, Name = "Twenty-one days until Christmas", Url = "", Date = new DateTime(2020, 12, 4) },
+                new Present { Id = 5, Name = "Twenty days until Christmas", Url = "", Date = new DateTime(2020, 12, 5) },
+                new Present { Id = 6, Name = "Nineteen days until Christmas", Url = "", Date = new DateTime(2020, 12, 6) },
+                new Present { Id = 7, Name = "Eighteen days until Christmas", Url = "", Date = new DateTime(2020, 12, 7) },
+                new Present { Id = 8, Name = "Seventeen days until Christmas", Url = "", Date = new DateTime(2020, 12, 8) },
+                new Present { Id = 9, Name = "Sixteen days until Christmas", Url = "", Date = new DateTime(2020, 12, 9) },
+                new Present { Id = 10, Name = "Fifteen days until Christmas", Url = "", Date = new DateTime(2020, 12, 10) },
+                new Present { Id = 11, Name = "Fourteen days until Christmas", Url = "", Date = new DateTime(2020, 12, 11) },
+                new Present { Id = 12, Name = "Thirteen days until Christmas", Url = "", Date = new DateTime(2020, 12, 12) },
+                new Present { Id = 13, Name = "Twelve days until Christmas", Url = "", Date = new DateTime(2020, 12, 13) },
+                new Present { Id = 14, Name = "Eleven days until Christmas", Url = "", Date = new DateTime(2020, 12, 14) },
+                new Present { Id = 15, Name = "Ten days until Christmas", Url = "", Date = new DateTime(2020, 12, 15) },
+                new Present { Id = 16, Name = "Nine days until Christmas", Url = "", Date = new DateTime(2020, 12, 16) },
+                new Present { Id = 17, Name = "Eight days until Christmas", Url = "", Date = new DateTime(2020, 12, 17) },
+                new Present { Id = 18, Name = "Seven days until Christmas", Url = "", Date = new DateTime(2020, 12, 18) },
+                new Present { Id = 19, Name = "Six days until Christmas", Url = "", Date = new DateTime(2020, 12, 19) },
+                new Present { Id = 20, Name = "Five days until Christmas", Url = "", Date = new DateTime(2020, 12, 20) },
+                new Present { Id = 21, Name = "Four days until Christmas", Url = "", Date = new DateTime(2020, 12, 21) },
+                new Present { Id = 22, Name = "Three days until Christmas", Url = "", Date = new DateTime(2020, 12, 22) },
+                new Present { Id = 23, Name = "Two days until Christmas", Url = "", Date = new DateTime(2020, 12, 23) },
+                new Present { Id = 24, Name = "One day until Christmas", Url = "", Date = new DateTime(2020, 12, 24) },
+                new Present { Id = 25, Name = "Christmas Day", Url = "", Date = new DateTime(2020, 12, 25) }
+            };
+
+            resources = new List<Resource>
+            {
+                new Resource { Url="https://dotnet.microsuntilt.com/learn/aspnet/blazor-tutorial/intro" },
+                new Resource { Url="https://jsakamoto.github.io/awesome-blazor-browser/" },
+                new Resource { Url="https://lupblazordemos.z13.web.core.windows.net/" },
+                new Resource { Url="https://github.com/aspnet/samples/tree/master/samples/aspnetcore/blazor/FlightFinder" },
+                new Resource { Url="https://github.com/dotnet-presentations/blazor-workshop/" },
+                new Resource { Url="https://blazorboilerplate.com/" },
+                new Resource { Url="https://docs.microsoft.com/en-us/aspnet/core/blazor/?view=aspnetcore-5.0" },
+                new Resource { Url="https://mudblazor.com/" },
+                new Resource { Url="https://en.wikipedia.org/wiki/Blazor" },
+                new Resource { Url="https://blazor.radzen.com/" }
             };
         }
 
@@ -25,7 +61,12 @@ namespace BlazorAdventCalendar.Services
 
         public IEnumerable<Present> GetPresents()
         {
-            return presents;
+            return presents.Where(p => p.Date <= DateTime.Today);
+        }
+
+        public IEnumerable<Resource> GetResources()
+        {
+            return resources;
         }
     }
 }
